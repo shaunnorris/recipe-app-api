31ab1fecf327250d4b6d69f2ed499a31d1e863e3
Shaun Norris <shaun@norrismail.net>
2020-11-20 13:39:09 +0800

    moved django secret key out of repo and rotated.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # recipe-app-api
 Recipe app api source code
+
+## securing env variables for passwords etc.
+create a .env file in the root of your project as follows:
+.env is ignored in .gitignore by default so you won't upload to github
+SECRET_KEY=[some random secure 50 char string]
+DB_NAME=[yours goes here]
+DB_USER=[yours goes here]
+DB_PASS=[yours goes here]
+DB_SERVICE=[yours goes here]
+DB_PORT=5432
--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't1$69p*7gwz1jt#-_tnt*e=b@m=5e&&%_9i)*5@5t#4&724)@m'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
