002daf8343a38d3f01cdcdea01d7299c5c1f0dd6
Shaun Norris <shaun@norrismail.net>
2020-11-20 14:58:53 +0800

    more tests and added flake8 to ghci

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,3 +4,8 @@
 def add(x, y):
     """Add two numbers together."""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,16 @@
 """Tests for the app app."""
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
 class CalcTests(TestCase):
+    """Container for Calculator tests."""
 
     def test_add_numbers(self):
         """Test that two numbers are added together."""
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned."""
+        self.assertEqual(subtract(5, 11), 6)
